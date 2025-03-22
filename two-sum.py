c7378b25c08fc6717677c776b01f3a7849d6dde7
greg ツ <66806458+creative-gregory@users.noreply.github.com>
2025-03-21 23:05:26 -0400

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,16 +1,41 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Example 1:
+        # Input: nums = [2,7,11,15], target = 9
+        # Output: [0,1]
+        # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        # Example 2:
+        
+        # Input: nums = [3,2,4], target = 6
+        # Output: [1,2]
+        # Example 3:
+        
+        # Input: nums = [3,3], target = 6
+        # Output: [0,1]
+
         # Brute Force Solution
         # for i in range(len(nums)):
         #     for j in range(i + 1, len(nums)):
         #         if nums[i] + nums[j] == target:
         #             return [i, j]
-
+        
         target_dict = dict()
 
         for index, num in enumerate(nums):
-            if (target - num) in target_dict:
-                return [target_dict[target - num], index]
-
+            difference = target - num
+            
+            if difference in target_dict:
+                return [target_dict[difference], index]
             else:
                 target_dict[num] = index
+
+        # Example Flow
+        # 1. Calculate the difference between the target and num[i]
+        # 2A. If the difference is not in the dict add the difference as the key and the index as the value
+        # 2B. If the difference is in the dict return the index value stored in the dict and the current index
+
+        # Example 1 Flow
+        # Difference: 9 - 2 == 7 
+        # Is 7 in the dict? no, add 7 to the dict as key and index == 0 as value
+        # Difference: 9 - 7 == 2
+        # Is 7 in the dict? yes return [taget_dict[num], index]
